854b8ba494580268a0d23d8b93d59a03c046ed37
ANSH PARASHAR <anshparasharap10@gmail.com>
2023-07-09 21:44:54 +0530

    new auth commit

--- src/components/Layout/Auth/LoginRegister.js
+++ src/components/Layout/Auth/LoginRegister.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
-import "./LoginStyle.css";
+// import "./LoginStyle.css";
+import "./LoginNew.css";
 import { Login } from "./Login";
 import { Register } from "./Register";
+
+import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
 
 const LoginRegister = () => {
   const [currentForm, setCurrentForm] = useState("login");
@@ -11,12 +14,141 @@ const LoginRegister = () => {
   };
 
   return (
-    <div className="LoginRegister">
-      {currentForm === "login" ? (
-        <Login onFormSwitch={toggleForm} />
-      ) : (
-        <Register onFormSwitch={toggleForm} />
-      )}
+    // <div className="LoginRegister">
+    //   {currentForm === "login" ? (
+    //     <Login onFormSwitch={toggleForm} />
+    //   ) : (
+    //     <Register onFormSwitch={toggleForm} />
+    //   )}
+    // </div>
+    <div className="loginBody">
+      <div className="section">
+        <div className="container">
+          <div className="row full-height justify-content-center">
+            <div className="col-12 text-center align-self-center py-5">
+              <div className="section pb-5 pt-5 pt-sm-2 text-center">
+                <h6 className="mb-0 pb-3">
+                  <span>Log In </span>
+                  <span>Sign Up</span>
+                </h6>
+                <input
+                  className="checkbox"
+                  type="checkbox"
+                  id="reg-log"
+                  name="reg-log"
+                />
+                <label htmlFor="reg-log" />
+                <div className="card-3d-wrap mx-auto">
+                  <div className="card-3d-wrapper">
+                    <div className="card-front">
+                      <div className="center-wrap">
+                        <div className="section text-center">
+                          <h4 className="mb-4 pb-3">Log In</h4>
+                          <div className="form-group">
+                            <input
+                              type="email"
+                              className="form-style"
+                              placeholder="Email"
+                            />
+                            <i className="input-icon uil uil-at" />
+                          </div>
+                          <div className="form-group mt-2">
+                            <input
+                              type="password"
+                              className="form-style"
+                              placeholder="Password"
+                            />
+                            <i className="input-icon uil uil-lock-alt" />
+                          </div>
+                          <a
+                            href="https://www.web-leb.com/code"
+                            className="LoginBtn btn mt-4"
+                          >
+                            Login
+                          </a>
+                          <GoogleOAuthProvider clientId="317641595846-22k5afqkbqr22b1ngrolbkpn95fo6hta.apps.googleusercontent.com">
+                            <GoogleLogin
+                              onSuccess={(credentialResponse) => {
+                                console.log(credentialResponse);
+                              }}
+                              onError={() => {
+                                console.log("Login Failed");
+                              }}
+                            />
+                          </GoogleOAuthProvider>
+                          <p className="mb-0 mt-4 text-center">
+                            <a
+                              href="https://www.web-leb.com/code"
+                              className="link"
+                            >
+                              Forgot your password?
+                            </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="card-back">
+                      <div className="center-wrap">
+                        <div className="section text-center">
+                          <h4 className="mb-3 pb-3">Sign Up</h4>
+                          <div className="form-group">
+                            <input
+                              type="text"
+                              className="form-style"
+                              placeholder="Full Name"
+                            />
+                            <i className="input-icon uil uil-user" />
+                          </div>
+                          <div className="form-group mt-2">
+                            <input
+                              type="tel"
+                              className="form-style"
+                              placeholder="Phone Number"
+                            />
+                            <i className="input-icon uil uil-phone" />
+                          </div>
+                          <div className="form-group mt-2">
+                            <input
+                              type="email"
+                              className="form-style"
+                              placeholder="Email"
+                            />
+                            <i className="input-icon uil uil-at" />
+                          </div>
+                          <div className="form-group mt-2">
+                            <input
+                              type="password"
+                              className="form-style"
+                              placeholder="Password"
+                            />
+                            <i className="input-icon uil uil-lock-alt" />
+                          </div>
+                          <a
+                            href="https://www.web-leb.com/code"
+                            className="LoginBtn btn mt-4"
+                          >
+                            Register
+                          </a>
+                          <GoogleOAuthProvider clientId="317641595846-22k5afqkbqr22b1ngrolbkpn95fo6hta.apps.googleusercontent.com">
+                            <GoogleLogin
+                              onSuccess={(credentialResponse) => {
+                                console.log(credentialResponse);
+                              }}
+                              onError={() => {
+                                console.log("Login Failed");
+                              }}
+                            />
+                          </GoogleOAuthProvider>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
